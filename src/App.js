668edd41d777d0df1c69f545c21511e8fca810b0
jdavesa <86001040+jdavesa@users.noreply.github.com>
2021-09-28 16:26:48 +0200

    JD - Featrues: Rutes App, distribución paginas y componentes.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import "./App.css"
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
+import Home from "./pages/home";
+import NewProduct from "./pages/new-product";
+import ProductsList from "./pages/products-list";
+import ProductDetails from "./pages/product-details";
+
+import Navbar from "./components/navbar";
+
 import * as PATHS from "./utils/paths";
 
 function App() {
   return (
     <div className="App">
+    <Navbar/>
       <Switch>
-        <Route exact path={PATHS.HOMEPAGE} component={HomePage} />
+        <Route exact path={PATHS.HOMEPAGE} component={Home} />
+        <Route exact path={PATHS.PRODUCTSLIST} component={ProductsList} />
+        <Route exact path={PATHS.PRODUCTDETAILS} component={ProductDetails} />
+        <Route exact path={PATHS.NEWPRODUCT} component={NewProduct} />
       </Switch>
     </div>
   );
