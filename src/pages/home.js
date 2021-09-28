6d733b4754fa50da5cb2c650206830b7e7514e48
jdavesa <86001040+jdavesa@users.noreply.github.com>
2021-09-28 20:14:33 +0200

    JD - Features: Form Test

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,32 @@
+import axios from "axios";
+import {useState} from 'react';
+
+const API_URL = "http://localhost:5005/signup";
+
+
 
 
 function Home(){
-    return(<h1>Hello World</h1>)
+
+    const[username, setUsername] = useState('')
+    
+    return(
+<div>
+    <form  onSubmit={(e)=>{
+        console.log('submit')
+        e.preventDefault();
+
+        let objectToSubmit = {username:username}
+
+        axios
+            .post("http://localhost:5005/signup", objectToSubmit)
+    }}>
+        <input type="text" name="username" value={username} onChange={(e)=>{
+            setUsername(e.target.value)
+        }}/>
+        <button type="submit">Calla la puta boca guiller</button>
+    </form>
+</div>)
 }
 
 export default Home
